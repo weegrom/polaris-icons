af1dd25cbcbce1d6484edc6ae2ee14eb95da59a3
Ben Scott <ben.scott@shopify.com>
2019-02-21 12:45:30 -0800

    Export icons in PascalCase
    JSX elements can refer to either intrinsic elements like `<div>` or
    user-defined components like `<MyComponent>`. JSX transpilation and
    Typescript both use the convention that user-defined components must
    start with a capital letter.
    
    Now that we are emiting components we should follow that convention. So
    rename `addMinor` to `AddMinor` etc so that consumers can do
    `<AddMinor>`.
    
    See https://www.typescriptlang.org/docs/handbook/jsx.html#type-checking

--- packages/polaris-icons/src/index.ts
+++ packages/polaris-icons/src/index.ts
@@ -1,207 +1,207 @@
 export {
-  default as addMinor,
+  default as AddMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/add_minor.svg';
 
 export {
-  default as alertMinor,
+  default as AlertMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/alert_minor.svg';
 
 export {
-  default as arrowDownMinor,
+  default as ArrowDownMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/arrow-down_minor.svg';
 
 export {
-  default as arrowLeftMinor,
+  default as ArrowLeftMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/arrow-left_minor.svg';
 
 export {
-  default as arrowRightMinor,
+  default as ArrowRightMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/arrow-right_minor.svg';
 
 export {
-  default as arrowUpDownMinor,
+  default as ArrowUpDownMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/arrow-up-down_minor.svg';
 
 export {
-  default as arrowUpMinor,
+  default as ArrowUpMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/arrow-up_minor.svg';
 
 export {
-  default as calendarMinor,
+  default as CalendarMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/calendar_minor.svg';
 
 export {
-  default as cancelSmallMinor,
+  default as CancelSmallMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/cancel-small_minor.svg';
 
 export {
-  default as cancelMajor,
+  default as CancelMajor,
 } from '@shopify/polaris-icons-raw/icons/polaris/cancel_major.svg';
 
 export {
-  default as caretDownMinor,
+  default as CaretDownMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/caret-down_minor.svg';
 
 export {
-  default as caretUpMinor,
+  default as CaretUpMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/caret-up_minor.svg';
 
 export {
-  default as checkmarkMinor,
+  default as CheckmarkMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/checkmark_minor.svg';
 
 export {
-  default as chevronDownMinor,
+  default as ChevronDownMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/chevron-down_minor.svg';
 
 export {
-  default as chevronLeftMinor,
+  default as ChevronLeftMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/chevron-left_minor.svg';
 
 export {
-  default as chevronRightMinor,
+  default as ChevronRightMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/chevron-right_minor.svg';
 
 export {
-  default as chevronUpMinor,
+  default as ChevronUpMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/chevron-up_minor.svg';
 
 export {
-  default as circleCancelMinor,
+  default as CircleCancelMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/circle-cancel_minor.svg';
 
 export {
-  default as circleChevronDownMinor,
+  default as CircleChevronDownMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/circle-chevron-down_minor.svg';
 
 export {
-  default as circleChevronLeftMinor,
+  default as CircleChevronLeftMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/circle-chevron-left_minor.svg';
 
 export {
-  default as circleChevronRightMinor,
+  default as CircleChevronRightMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/circle-chevron-right_minor.svg';
 
 export {
-  default as circleChevronUpMinor,
+  default as CircleChevronUpMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/circle-chevron-up_minor.svg';
 
 export {
-  default as circleInformationMajor,
+  default as CircleInformationMajor,
 } from '@shopify/polaris-icons-raw/icons/polaris/circle-information_major.svg';
 
 export {
-  default as circlePlusMinor,
+  default as CirclePlusMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/circle-plus_minor.svg';
 
 export {
-  default as circlePlusOutlineMinor,
+  default as CirclePlusOutlineMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/circle-plus-outline_minor.svg';
 
 export {
-  default as conversationMinor,
+  default as ConversationMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/conversation_minor.svg';
 
 export {
-  default as deleteMinor,
+  default as DeleteMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/delete_minor.svg';
 
 export {
-  default as disableMinor,
+  default as DisableMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/disable_minor.svg';
 
 export {
-  default as disputeMinor,
+  default as DisputeMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/dispute_minor.svg';
 
 export {
-  default as duplicateMinor,
+  default as DuplicateMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/duplicate_minor.svg';
 
 export {
-  default as embedMinor,
+  default as EmbedMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/embed_minor.svg';
 
 export {
-  default as exportMinor,
+  default as ExportMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/export_minor.svg';
 
 export {
-  default as externalMinor,
+  default as ExternalMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/external_minor.svg';
 
 export {
-  default as helpMajor,
+  default as HelpMajor,
 } from '@shopify/polaris-icons-raw/icons/polaris/help_major.svg';
 
 export {
-  default as homeMajor,
+  default as HomeMajor,
 } from '@shopify/polaris-icons-raw/icons/polaris/home_major.svg';
 
 export {
-  default as horizontalDotsMinor,
+  default as HorizontalDotsMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/horizontal-dots_minor.svg';
 
 export {
-  default as importMinor,
+  default as ImportMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/import_minor.svg';
 
 export {
-  default as logOutMinor,
+  default as LogOutMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/log-out_minor.svg';
 
 export {
-  default as menuMajor,
+  default as MenuMajor,
 } from '@shopify/polaris-icons-raw/icons/polaris/menu_major.svg';
 
 export {
-  default as notesMinor,
+  default as NotesMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/notes_minor.svg';
 
 export {
-  default as notificationMajor,
+  default as NotificationMajor,
 } from '@shopify/polaris-icons-raw/icons/polaris/notification_major.svg';
 
 export {
-  default as onlineStoreMajor,
+  default as OnlineStoreMajor,
 } from '@shopify/polaris-icons-raw/icons/polaris/online-store_major.svg';
 
 export {
-  default as ordersMajor,
+  default as OrdersMajor,
 } from '@shopify/polaris-icons-raw/icons/polaris/orders_major.svg';
 
 export {
-  default as printMinor,
+  default as PrintMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/print_minor.svg';
 
 export {
-  default as productsMajor,
+  default as ProductsMajor,
 } from '@shopify/polaris-icons-raw/icons/polaris/products_major.svg';
 
 export {
-  default as profileMinor,
+  default as ProfileMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/profile_minor.svg';
 
 export {
-  default as refreshMinor,
+  default as RefreshMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/refresh_minor.svg';
 
 export {
-  default as riskMinor,
+  default as RiskMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/risk_minor.svg';
 
 export {
-  default as saveMinor,
+  default as SaveMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/save_minor.svg';
 
 export {
-  default as searchMinor,
+  default as SearchMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/search_minor.svg';
 
 export {
-  default as subtractMinor,
+  default as SubtractMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/subtract_minor.svg';
 
 export {
-  default as viewMinor,
+  default as ViewMinor,
 } from '@shopify/polaris-icons-raw/icons/polaris/view_minor.svg';
