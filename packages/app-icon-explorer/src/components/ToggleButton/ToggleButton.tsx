e036e25a6d1745054aa00aa030ede0d87d1ca37d
Ben Scott <ben.scott@shopify.com>
2019-03-27 11:41:26 -0700

    Remove undesired styles and logic from ToggleButton

--- packages/app-icon-explorer/src/components/ToggleButton/ToggleButton.tsx
+++ packages/app-icon-explorer/src/components/ToggleButton/ToggleButton.tsx
@@ -1,26 +1,13 @@
 import * as React from 'react';
 import classNames from 'classnames';
 
-import {
-  Icon,
-  IconProps,
-  withAppProvider,
-  WithAppProviderProps,
-} from '@shopify/polaris';
-
 import styles from './ToggleButton.module.scss';
-
-export type IconSource = IconProps['source'];
 
 export interface Props {
   children?: string | string[];
   id?: string;
-  primary?: boolean;
   disabled?: boolean;
   fullWidth?: boolean;
-  loading?: boolean;
-  outline?: boolean;
-  icon?: React.ReactNode | IconSource;
   accessibilityLabel?: string;
   ariaControls?: string;
   pressed?: boolean;
@@ -32,12 +19,9 @@ export interface Props {
   onKeyDown?(event: React.KeyboardEvent<HTMLButtonElement>): void;
 }
 
-type CombinedProps = Props & WithAppProviderProps;
-
-function ToggleButton({
+export default function ToggleButton({
   id,
   disabled,
-  loading,
   children,
   accessibilityLabel,
   ariaControls,
@@ -48,31 +32,14 @@ function ToggleButton({
   onKeyDown,
   onKeyPress,
   onKeyUp,
-  icon,
-  primary,
-  outline,
   fullWidth,
-}: CombinedProps) {
+}: Props) {
   const className = classNames(
     styles.ToggleButton,
-    primary && styles.primary,
-    outline && styles.outline,
     disabled && styles.disabled,
     pressed && styles.pressed,
     fullWidth && styles.fullWidth,
-    icon && children == null && styles.iconOnly,
   );
-
-  let iconMarkup;
-
-  if (icon) {
-    const iconInner = isIconSource(icon) ? (
-      <Icon source={loading ? 'placeholder' : icon} />
-    ) : (
-      icon
-    );
-    iconMarkup = <IconWrapper>{iconInner}</IconWrapper>;
-  }
 
   return (
     <button
@@ -90,26 +57,13 @@ function ToggleButton({
       aria-label={accessibilityLabel}
       aria-controls={ariaControls}
       aria-pressed={pressed}
-      role={loading ? 'alert' : undefined}
-      aria-busy={loading ? true : undefined}
     >
       <span className={styles.Content}>
-        {iconMarkup && iconMarkup}
         {children && <span className={styles.Text}>{children}</span>}
       </span>
     </button>
   );
 }
-
-export function IconWrapper({children}: any) {
-  return <span className={styles.Icon}>{children}</span>;
-}
-
-function isIconSource(x: any): x is IconSource {
-  return typeof x === 'string' || (typeof x === 'object' && x.body);
-}
-
-export default withAppProvider<Props>()(ToggleButton);
 
 function handleMouseUpByBlurring({
   currentTarget,
